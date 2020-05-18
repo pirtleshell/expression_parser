dd0cf56bc8220af54c1286e88966fa99937e1bda
Robert Pirtle <astropirtle@gmail.com>
2020-05-17 17:27:13 -0700

    use loop keyword

--- src/bin.rs
+++ src/bin.rs
@@ -5,7 +5,7 @@ use parser::Parser;
 
 fn main() {
     println!("Evaluate some expressions!");
-    while true {
+    loop {
         let mut input: String = String::new();
         std::io::stdin().read_line(&mut input).expect("failed to read");
         println!("{}", Parser::evaluate(&input));
